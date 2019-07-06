c80363df56b2500799f506cbf5631b1a06225f9e
Mohammed Al-Dahleh <maldahleh@gmail.com>
2019-07-06 14:16:46 -0400

    Define token type

---
+++ token/token.go
@@ -0,0 +1,27 @@
+package token
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	IDENT = "IDENT"
+	INT = "INT"
+
+	ASSIGN = "="
+	PLUS = "+"
+
+	COMMA = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+)
+
+type TokenType string
+
+type Token struct {
+	Type TokenType
+	Literal string
+}
